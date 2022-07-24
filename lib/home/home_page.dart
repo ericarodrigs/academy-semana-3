44bb66eeae31b81d9b94146d54c88040b5d59a02
Ventura08 <estevaotevu08@gmail.com>
2022-07-24 17:02:06 -0300

    consertando next e done dos formsField

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -9,6 +9,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GenderIdentity gender = GenderIdentity.masculino;
+  bool isChecked = false;
+  String dropdownValue = 'Flutter';
+
+  InputBorder inputBorder = const OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black, width: 1),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +30,267 @@ class _HomePageState extends State<HomePage> {
         ),
         centerTitle: true,
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 12),
+              margin: const EdgeInsets.all(12),
+              child: const Text(
+                'Criar Conta',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 12, right: 12),
+              margin: const EdgeInsets.all(12),
+              child: TextFormField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                    labelText: 'Nome',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    enabledBorder: inputBorder,
+                    disabledBorder: inputBorder,
+                    focusedBorder: inputBorder),
+                textInputAction: TextInputAction.next,
+                onEditingComplete: () => FocusScope.of(context).nextFocus(),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 12, right: 12),
+              margin: const EdgeInsets.all(12),
+              child: TextFormField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    enabledBorder: inputBorder,
+                    disabledBorder: inputBorder,
+                    focusedBorder: inputBorder),
+                textInputAction: TextInputAction.next,
+                onEditingComplete: () => FocusScope.of(context).nextFocus(),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 12, right: 12),
+              margin: const EdgeInsets.all(12),
+              child: TextFormField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                    labelText: 'Senha',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    enabledBorder: inputBorder,
+                    disabledBorder: inputBorder,
+                    focusedBorder: inputBorder),
+                textInputAction: TextInputAction.next,
+                onEditingComplete: () => FocusScope.of(context).nextFocus(),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 12, right: 12),
+              margin: const EdgeInsets.all(12),
+              child: TextFormField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                    labelText: 'Confirmar Senha',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    enabledBorder: inputBorder,
+                    disabledBorder: inputBorder,
+                    focusedBorder: inputBorder),
+                textInputAction: TextInputAction.next,
+                onEditingComplete: () => FocusScope.of(context).nextFocus(),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 12, right: 12),
+              margin: const EdgeInsets.all(12),
+              child: TextFormField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                    labelText: 'CPF',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    enabledBorder: inputBorder,
+                    disabledBorder: inputBorder,
+                    focusedBorder: inputBorder),
+                textInputAction: TextInputAction.next,
+                onEditingComplete: () => FocusScope.of(context).nextFocus(),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 12, right: 12),
+              margin: const EdgeInsets.all(12),
+              child: TextFormField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                    labelText: 'Data de Nascimento',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    enabledBorder: inputBorder,
+                    disabledBorder: inputBorder,
+                    focusedBorder: inputBorder),
+                textInputAction: TextInputAction.done,
+                onEditingComplete: () => FocusScope.of(context).unfocus(),
+              ),
+            ),
+            Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(left: 24, top: 12, bottom: 12),
+                  child: Text(
+                    'Identidade de Gênero',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                ListTile(
+                  dense: true,
+                  visualDensity:
+                      const VisualDensity(vertical: -3, horizontal: -4),
+                  title: const Text(
+                    'Masculino',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  leading: Radio<GenderIdentity>(
+                    value: GenderIdentity.masculino,
+                    groupValue: gender,
+                    onChanged: (GenderIdentity? genderSelected) {
+                      setState(() {
+                        gender = genderSelected!;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  dense: true,
+                  visualDensity:
+                      const VisualDensity(vertical: -3, horizontal: -4),
+                  title: const Text(
+                    'Feminino',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  leading: Radio<GenderIdentity>(
+                    value: GenderIdentity.feminino,
+                    groupValue: gender,
+                    onChanged: (GenderIdentity? genderSelected) {
+                      setState(() {
+                        gender = genderSelected!;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  dense: true,
+                  visualDensity:
+                      const VisualDensity(vertical: -3, horizontal: -4),
+                  title: const Text(
+                    'Outro',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  leading: Radio<GenderIdentity>(
+                    value: GenderIdentity.outro,
+                    groupValue: gender,
+                    onChanged: (GenderIdentity? genderSelected) {
+                      setState(() {
+                        gender = genderSelected!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(left: 24, top: 12, bottom: 12),
+                  child: Text(
+                    'Turma',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 24, right: 24),
+              child: InputDecorator(
+                decoration: const InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1),
+                )),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton(
+                      isExpanded: true,
+                      isDense: true,
+                      value: dropdownValue,
+                      items: <String>['Flutter', 'NodeJS', 'QA']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      }),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Row(
+                children: [
+                  Checkbox(
+                      value: isChecked,
+                      onChanged: (value) {
+                        setState(() {
+                          isChecked = !isChecked;
+                        });
+                      }),
+                  const Text(
+                    'Concordo com os termos.',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 24),
+              child: SizedBox(
+                width: 240,
+                height: 48,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Criar Conta',
+                      style: TextStyle(fontSize: 18),
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+enum GenderIdentity { masculino, feminino, outro }
