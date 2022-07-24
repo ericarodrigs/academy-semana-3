434b98d39c083f0f1a907e774a0c1611624d3ee7
Erica Rodrigues <ericamrodrigs@gmail.com>
2022-07-23 21:15:12 -0300

    building the layout without style

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -9,6 +9,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GenderIdentity gender = GenderIdentity.masculino;
+  bool isChecked = false;
+  String dropdownValue = 'Flutter';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +26,98 @@ class _HomePageState extends State<HomePage> {
         ),
         centerTitle: true,
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text('Criar Conta'),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Nome'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Senha'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Confirmar Senha'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'CPF'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Data de Nascimento'),
+            ),
+            Text('Identidade de Gênero'),
+            ListTile(
+              title: const Text('Masculino'),
+              leading: Radio<GenderIdentity>(
+                value: GenderIdentity.masculino,
+                groupValue: gender,
+                onChanged: (GenderIdentity? genderSelected) {
+                  setState(() {
+                    gender = genderSelected!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Feminino'),
+              leading: Radio<GenderIdentity>(
+                value: GenderIdentity.feminino,
+                groupValue: gender,
+                onChanged: (GenderIdentity? genderSelected) {
+                  setState(() {
+                    gender = genderSelected!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Outro'),
+              leading: Radio<GenderIdentity>(
+                value: GenderIdentity.outro,
+                groupValue: gender,
+                onChanged: (GenderIdentity? genderSelected) {
+                  setState(() {
+                    gender = genderSelected!;
+                  });
+                },
+              ),
+            ),
+            Text('Turma'),
+            DropdownButton(
+                value: dropdownValue,
+                items: <String>['Flutter', 'NodeJS', 'QA']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                }),
+            Row(
+              children: [
+                Checkbox(
+                    value: isChecked,
+                    onChanged: (value) {
+                      setState(() {
+                        isChecked = !isChecked;
+                      });
+                    }),
+                Text('Concordo com os termos.'),
+              ],
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Criar Conta')),
+          ],
+        ),
+      ),
     );
   }
 }
+
+enum GenderIdentity { masculino, feminino, outro }
