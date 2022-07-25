4ea2d24b319d2daac766f112a312eb638538b996
AndreHabib <andre.habib.017@ufrn.edu.br>
2022-07-25 09:57:01 -0300

    :twisted_rightwards_arrows: Merge of feature/validatePassword done successfully

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -107,7 +107,6 @@ class _HomePageState extends State<HomePage> {
                     } else {
                       return null;
                     }
-                    ;
                   },
                   cursorColor: Colors.black,
                   decoration: InputDecoration(
