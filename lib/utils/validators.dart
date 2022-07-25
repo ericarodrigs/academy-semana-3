ab5d190113912cb35f0fe8548af113838777b96e
Ventura08 <estevaotevu08@gmail.com>
2022-07-24 21:37:52 -0300

    validateEmail completo

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -1,22 +1,15 @@
-class Validators {
-  Validators();
-  // ignore: todo
-  /* TODO: Nome
-RN01: o campo não pode ser vazio
-RN02: o campo não pode ter mais de 1 espaço entre as palavras
-RN03: ao digitar, a primeira letra de cada palavra deve ser maiúscula
-RN04: abreviações com ponto não devem ser aceitas */
+import 'package:raro_buddy_group_4/utils/regex.dart';
 
+class Validators extends RegexTypes {
+  Validators();
   static String? validateName(String? value) {
-    RegExp upperCaseValidator = RegExp(r'[A-Z]');
-    RegExp hasSymbol = RegExp(r'[!@#<>?.":_`~;[\]\\|=+)(*&^%0-9-]');
     if (value == null || value.isEmpty) {
       return 'Opa, voce precisa digitar o seu nome!';
-    } else if (!value.contains('  ') && !hasSymbol.hasMatch(value)) {
+    } else if (!value.contains('  ') && !RegexTypes.hasSymbol.hasMatch(value)) {
       // print(valu
       final List<String> isUpper = value.split(' ');
       for (int i = 0; i < isUpper.length; i++) {
-        if (!upperCaseValidator.hasMatch(isUpper[i][0])) {
+        if (!RegexTypes.upperCaseValidator.hasMatch(isUpper[i][0])) {
           return 'Opa, a palavra "${isUpper[i]}" deve começar com letra maiuscula';
         }
       }
@@ -27,6 +20,13 @@ RN04: abreviações com ponto não devem ser aceitas */
   }
 
   static String? validateEmail(String? value) {
-    print('ola');
+    if (value == null || value.isEmpty) {
+      return 'Ou, o campo de email não pode ser vazio';
+    } else if (!RegexTypes.emailValidator.hasMatch(value) ||
+        value.contains(' ') ||
+        RegexTypes.upperCaseValidator.hasMatch(value)) {
+      return 'Cara, digita um email válido';
+    }
+    return null;
   }
 }
